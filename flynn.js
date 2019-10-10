31cfa40d6c2d9241b7bad254367fd3eacee592b9
DJTrog <djohnson3535@gmail.com>
2019-10-09 22:30:38 -0700

    major changes new hero

--- flynn.js
+++ flynn.js
@@ -1,8 +1,24 @@
-$(function() {
+/*$(function() {
   $('.js-nav a, .js-connect').click(function(e) {
     e.preventDefault();
     $('body, html').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
     }, 750);
   });
+});*/
+
+/*back to top code*/
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
 });
